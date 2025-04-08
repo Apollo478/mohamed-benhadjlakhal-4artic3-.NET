40ee4443e2286076340222d077575a79d380ff7c
mohamed28875701 <mohamedbenhadjlakhal@gmail.com>
2025-04-08 08:54:57 +0100

    FEAT: LAB

---
+++ Examen.ApplicationCore/Domain/Laboratoire.cs
@@ -0,0 +1,11 @@
+namespace Examen.ApplicationCore.Domain;
+
+public class Laboratoire
+{
+    public string Intitule { get; set; }
+    public int LaboratoireId { get; set; }
+    public string Localisation { get; set; }
+    
+    
+    
+}
